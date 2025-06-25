50195c5de9c27591085b59c80a525fd41465df52
Edo Arya <edoarya@mac.local>
2025-06-26 01:00:29 +0700

    ab-11

--- ViewModels/Dashboard/DashboardViewModel.cs
+++ ViewModels/Dashboard/DashboardViewModel.cs
@@ -1,3 +1,48 @@
+// // ViewModels/Dashboard/DashboardViewModel.cs
+// namespace AspnetCoreMvcFull.ViewModels.Dashboard
+// {
+//   public class DashboardViewModel
+//   {
+//     public string SelectedPeriod { get; set; } = "month"; // Default: bulan ini
+
+//     // Summary metrics untuk seluruh crane
+//     public DashboardMetricsViewModel SummaryMetrics { get; set; } = new DashboardMetricsViewModel();
+
+//     // Data crane individu untuk chart
+//     public List<CraneMetricsViewModel> CraneMetrics { get; set; } = new List<CraneMetricsViewModel>();
+
+//     // Statistik crane
+//     public CraneStatisticsViewModel CraneStatistics { get; set; } = new CraneStatisticsViewModel();
+//   }
+
+//   public class DashboardMetricsViewModel
+//   {
+//     public double AvailabilityPercentage { get; set; }
+//     public double UtilisationPercentage { get; set; }
+//     public double UsagePercentage { get; set; }
+//   }
+
+//   public class CraneMetricsViewModel
+//   {
+//     public string Code { get; set; } = string.Empty;
+//     public double AvailabilityPercentage { get; set; }
+//     public double UtilisationPercentage { get; set; }
+//     public double UsagePercentage { get; set; }
+//   }
+
+//   public class CraneStatisticsViewModel
+//   {
+//     public int TotalCranes { get; set; }
+//     public int OperationalCranes { get; set; }
+//     public int StandbyCranes { get; set; }
+//     public int MaintenanceCranes { get; set; }
+
+//     public double OperationalPercentage => TotalCranes > 0 ? (double)OperationalCranes / TotalCranes * 100 : 0;
+//     public double StandbyPercentage => TotalCranes > 0 ? (double)StandbyCranes / TotalCranes * 100 : 0;
+//     public double MaintenancePercentage => TotalCranes > 0 ? (double)MaintenanceCranes / TotalCranes * 100 : 0;
+//   }
+// }
+
 // ViewModels/Dashboard/DashboardViewModel.cs
 namespace AspnetCoreMvcFull.ViewModels.Dashboard
 {
@@ -13,6 +58,9 @@ namespace AspnetCoreMvcFull.ViewModels.Dashboard
 
     // Statistik crane
     public CraneStatisticsViewModel CraneStatistics { get; set; } = new CraneStatisticsViewModel();
+
+    // Statistik booking
+    public BookingStatisticsViewModel BookingStatistics { get; set; } = new BookingStatisticsViewModel();
   }
 
   public class DashboardMetricsViewModel
@@ -40,5 +88,15 @@ namespace AspnetCoreMvcFull.ViewModels.Dashboard
     public double OperationalPercentage => TotalCranes > 0 ? (double)OperationalCranes / TotalCranes * 100 : 0;
     public double StandbyPercentage => TotalCranes > 0 ? (double)StandbyCranes / TotalCranes * 100 : 0;
     public double MaintenancePercentage => TotalCranes > 0 ? (double)MaintenanceCranes / TotalCranes * 100 : 0;
+  }
+
+  public class BookingStatisticsViewModel
+  {
+    public int TotalBookings { get; set; }
+    public int CancelledBookings { get; set; }
+    public int OngoingBookings { get; set; }
+
+    public double CancelledPercentage => TotalBookings > 0 ? (double)CancelledBookings / TotalBookings * 100 : 0;
+    public double OngoingPercentage => TotalBookings > 0 ? (double)OngoingBookings / TotalBookings * 100 : 0;
   }
 }
